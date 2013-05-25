7324dfa52a02e7995e8187992be7f26c320858f7
FantasticMrFFox <lavelle.hurley@googlemail.com>
2013-05-25 09:45:23 +0100

    files

---
+++ Innovation-App/scripts/js/scripts.js
@@ -0,0 +1,38 @@
+/*
+function printValue() {
+	//document.getElementById("valueInput").innerHTML(value);
+	//document.getElementById("valueInput").innerHTML = element.value;
+	//document.getElementById("valueInput").innerHTML = work.value;
+	//alert(document.getElementById("inputField").value);
+	alert(document.getElementById("inputField").value + " hello");
+	}
+*/	
+	
+function printValue() {
+	alert(document.getElementById("inputField").value + " hello");
+	}
+
+
+
+
+function split()
+{
+
+var str = 'something -- something_else';
+var substr = str.split(' -- ');
+// substr[0] contains "something"
+// substr[1] contains "something_else"
+}
+
+
+
+
+
+
+
+
+
+var string = "this is a string,and I have a comma";
+var array = string.split(/[\s,]+/);
+
+
